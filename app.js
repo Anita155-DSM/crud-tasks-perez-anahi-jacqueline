f6ddcb0ece870d5ead3de7e3612bf9bf6158cbe4
Anita-DSM <anahiperezdsm@gmail.com>
2025-08-08 07:43:35 -0300

    Realizo correciones en database.js así tener exito en la conexion con la base de datos, también realizo los modelos de User y Task en user.models.js y task.models.js. Realizo cambios en app.js y levanto correctamente el servidor.

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 import express from 'express';
+import { sequelize } from './src/config/database.js';
+import { User } from './src/models/user.models.js';
+import { Task } from './src/models/task.models.js';
+
 const app = express();
+const PORT=process.env.PORT || 3306;
 
 app.use(express.json());
-
-// (Ruta base de prueba nomas)
 app.get('/', (req, res) => {
   res.send('API funcionando correctamente');
+});
+
+// Intentamos conectar a la base de datos
+try {
+  await sequelize.authenticate();
+  console.log('Conexión a la base de datos establecida correctamente');
+
+  // crea las tablas si no existen
+  await sequelize.sync({ alter: true });
+  console.log('📦 Tablas sincronizadas correctamente');
+} catch (error) {
+  console.error(' Error al conectar con la base de datos:', error);
+}
+
+// levantamos servidor
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
 });
--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,14 @@
 import { Sequelize } from "sequelize";
-import dotenv from dotenv;
+import dotenv from 'dotenv';  //correccion 
+import MysqlDialect from "sequelize/lib/dialects/mysql/index";
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_NAME,
-process.env.DB_USER, process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT
-    }
-)
+export const sequelize = new Sequelize(
+  process.env.DB_NAME, //ordeno mejor esto, sino es un poco confuso
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT //correccion
+  }
+);
