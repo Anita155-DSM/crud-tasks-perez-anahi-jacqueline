fdcf093a119798678c2f679ce2f5d67b0e75b316
Anita-DSM <anahiperezdsm@gmail.com>
2025-08-25 00:10:05 -0300

    feat: agregar validaciones de usuario, borrado lógico y archivos base para validaciones

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -5,8 +5,10 @@ import { ProfileModel } from './profile.models.js';
 export const User = sequelize.define('User', {
   name: { type: DataTypes.STRING(100), allowNull: false },
   email: { type: DataTypes.STRING(100), allowNull: false, unique: true },
-  password: { type: DataTypes.STRING(100), allowNull: false }
+  password: { type: DataTypes.STRING(100), allowNull: false },
+  is_deleted: { type: DataTypes.BOOLEAN, defaultValue: false }
 }, { timestamps: false });
+
 
 // Solo aquí definimos la relación 1:1 (uno a uno)
 User.hasOne(ProfileModel, { foreignKey: 'user_id', as: 'profile' });
